80c5255a62be02bd3f98cb26d5811676442f426d
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2024-12-13 15:33:41 +0700

    registry contract

--- src/MaglevEulerSwapRegistry.sol
+++ src/MaglevEulerSwapRegistry.sol
@@ -10,19 +10,54 @@ import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 contract MaglevEulerSwapRegistry is Ownable {
     event PoolRegistered(address indexed asset0, address indexed asset1, uint256 indexed fee, address pool);
 
+    error InvalidQuery();
+    error PoolAlreadyRegistered();
+
+    /// @dev An array to store all pools addresses.
+    address[] public allPools;
+    /// @dev Mapping from asset0/asset1/fee => pool address.
     mapping(address => mapping(address => mapping(uint256 => address))) public getPool;
 
     constructor() Ownable(msg.sender) {}
 
+    /// @notice Register a deployed pool address.
+    /// @param _pool Pool's address.
     function registerPool(address _pool) external onlyOwner {
         address asset0 = IMaglevEulerSwap(_pool).asset0();
         address asset1 = IMaglevEulerSwap(_pool).asset1();
         uint256 fee = IMaglevEulerSwap(_pool).fee();
 
+        require(getPool[asset0][asset1][fee] == address(0), PoolAlreadyRegistered());
+
         getPool[asset0][asset1][fee] = _pool;
         // populate mapping in the reverse direction, deliberate choice to avoid the cost of comparing addresses
-        getPool[asset0][asset1][fee] = _pool;
+        getPool[asset1][asset0][fee] = _pool;
+
+        allPools.push(_pool);
 
         emit PoolRegistered(asset0, asset1, fee, _pool);
+    }
+
+    /// @notice Get the length of `allPools` array.
+    /// @return `allPools` length.
+    function allPoolsLength() external view returns (uint256) {
+        return allPools.length;
+    }
+
+    /// @notice Get a slice of the registered pools array.
+    /// @param _start Start index of the slice.
+    /// @param _end End index of the slice.
+    /// @return An array containing the slice of the registered pools.
+    function getAllPoolsListSlice(uint256 _start, uint256 _end) external view returns (address[] memory) {
+        uint256 length = allPools.length;
+        if (_end == type(uint256).max) _end = length;
+        if (_end < _start || _end > length) revert InvalidQuery();
+
+        address[] memory allPoolsList = new address[](_end - _start);
+        for (uint256 i; i < _end - _start; ++i) {
+            allPoolsList[i] = allPools[_start + i];
+        }
+
+        return allPoolsList;
     }
 }
