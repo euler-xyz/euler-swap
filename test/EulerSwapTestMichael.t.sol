59b40dd41dac63bcfc69cca3c78bf55af9430935
Michael Bentley <michael@euler.xyz>
2025-02-27 20:45:08 +0000

    add new test file

---
+++ test/EulerSwapTestMichael.t.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.24;
+
+import {IEVault, IEulerSwap, EulerSwapTestBase, EulerSwap, TestERC20} from "./EulerSwapTestBase.t.sol";
+
+contract EulerSwapTest is EulerSwapTestBase {
+    EulerSwap public eulerSwap;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        eulerSwap = createEulerSwap(50e18, 50e18, 0, 1e18, 1e18, 0.4e18, 0.85e18);
+    }
+
+    function test_basicSwap_exactIn() public monotonicHolderNAV {
+        uint256 amountIn = 1e18;
+        uint256 amountOut =
+            periphery.quoteExactInput(address(eulerSwap), address(assetTST), address(assetTST2), amountIn);
+        assertApproxEqAbs(amountOut, 0.9974e18, 0.0001e18);
+
+        assetTST.mint(address(this), amountIn);
+
+        assetTST.transfer(address(eulerSwap), amountIn);
+        eulerSwap.swap(0, amountOut, address(this), "");
+
+        assertEq(assetTST2.balanceOf(address(this)), amountOut);
+    }
+
+    function test_basicSwap_exactOut() public monotonicHolderNAV {
+        uint256 amountOut = 1e18;
+        uint256 amountIn =
+            periphery.quoteExactOutput(address(eulerSwap), address(assetTST), address(assetTST2), amountOut);
+        assertApproxEqAbs(amountIn, 1.0025e18, 0.0001e18);
+
+        assetTST.mint(address(this), amountIn);
+
+        assetTST.transfer(address(eulerSwap), amountIn);
+        eulerSwap.swap(0, amountOut, address(this), "");
+
+        assertEq(assetTST2.balanceOf(address(this)), amountOut);
+    }
+
+}
