ddea00366769cfde5112d4cd4968c3e947d228c7
Michael Bentley <michael@euler.xyz>
2025-03-27 21:32:25 +0000

    remove old functions

--- test/EulerSwapInverseTeat.t.sol
+++ test/EulerSwapInverseTeat.t.sol
@@ -18,43 +18,6 @@ contract EulerSwapScenarioTest is Test {
         return Math.mulDiv(1e18 - c, 1e18, x, Math.Rounding.Ceil) + Math.mulDiv(c, 2e18 - x, 1e18, Math.Rounding.Ceil);
     }
 
-    function dfdx(uint256 x, uint256 c) internal pure returns (uint256) {
-        return Math.mulDiv(1e18 - c, 1e36, x * x, Math.Rounding.Ceil) + c;
-    }
-
-    function newtons(uint256 y, uint256 c, uint256 xMin, uint256 xMax, uint256 yCalc) internal pure returns (uint256) {        
-        uint256 x = xMax;
-        for (uint256 i = 0; i < 10; i++) {
-            int256 yDiff;
-            if (i == 0) {
-                // we already have a y-calculation to re-use on first iteration and know that x is not yet a lower bound
-                yDiff = int256(yCalc) - int256(y);
-            } else {
-                yCalc = f(x, c);
-                yDiff = int256(yCalc) - int256(y);
-                if (yDiff == 0) {
-                    return x;
-                } else if (yDiff > 0) {
-                    xMin = x; // only want x-calculations that are <= the true solution
-                }
-            }
-            int256 xN = int256(x) + (yDiff * (1e18 - 1) / 1e18) / int256(dfdx(x, c));
-            if (xN == int256(x)) {
-                return xMin;
-            } else if (xN < int256(xMin)) {
-                x = xMin;
-            } else if (xN >= int256(xMin) && xN <= 1e18) {                
-                x = uint256(xN);
-            }
-            console.log();
-        }
-        console.log();
-    }
-
-    function secant(uint256 y, uint256 c, uint256 xMin, uint256 xMax, uint256 yCalc) internal pure returns (uint256) {
-        return xMin + uint256((int256(yCalc) - int256(y)) * int256(xMax - xMin) / (int256(yCalc) - int256(f(xMax, c))));
-    }
-
     function abs(int256 x) internal pure returns (uint256) {
         return uint256(x >= 0 ? x : -x);
     }
@@ -74,7 +37,6 @@ contract EulerSwapScenarioTest is Test {
     // Note: second if statement fixes off-by-one error
     // if xMin == xMax - 1 and and y >= f(xMin, c) is true, then xMid = (xMin + xMax) / 2 = xMin and xMax = xMid = xMin, but we never tested xMax
     function binary(uint256 y, uint256 c, uint256 xMin, uint256 xMax) internal pure returns (uint256) {
-        uint256 count = 0;
         if(xMin < 1) {
             xMin = 1;
         }        
@@ -91,30 +53,6 @@ contract EulerSwapScenarioTest is Test {
             xMin += 1;
         }
         return xMin;
-    }
-
-    function fInverse(uint256 y, uint256 c) internal pure returns (uint256) {
-        uint256 x = quadratic(y, c); // get an initial x
-        uint256 yCalc = f(x, c); // calculate the y given the x
-        if (y == yCalc) {
-            return x; // quadratic worked exactly
-        } else {
-            uint256 xMin = (1e18 - c) * 1e18 / y; // get an initial lower bound
-            xMin = xMin < 1 ? 1 : xMin;
-            uint256 xMax = 1e18;
-
-            if (y > yCalc) {
-                console.log("newtwons");
-                xMax = x; // quadratic slightly over-estimated x, so use as upper bound
-                xMin = newtons(y, c, xMin, xMax, yCalc); // get a nearby lower bound using Newton's method
-            } else {
-                console.log("secant");
-                xMin = x; // quadratic slightly under-estimated x, so use as lower bound
-                xMax = secant(y, c, xMin, xMax, yCalc); // get a nearby upper bound using secant method
-            }
-
-            return binary(y, c, xMin, xMax); // refine the bounds using binary search
-        }
     }
 
     function test_Binary() public {
