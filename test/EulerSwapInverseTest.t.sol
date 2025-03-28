99585cc0b9b01157778f3e86334fd9fd6ce98aa2
Michael Bentley <michael@euler.xyz>
2025-03-28 12:49:23 +0000

    fix: quadratic working with 100,000 fuzz tests

--- test/EulerSwapInverseTest.t.sol
+++ test/EulerSwapInverseTest.t.sol
@@ -4,13 +4,23 @@ pragma solidity ^0.8.24;
 import "forge-std/Test.sol";
 import "forge-std/console.sol"; // Import console.sol for logging
 import {Math} from "openzeppelin-contracts/utils/math/Math.sol";
-import { PRBMathUD60x18 } from "prb-math/PRBMathUD60x18.sol";
+import {UD60x18, ud} from "prb-math/UD60x18.sol";
 
 contract EulerSwapScenarioTest is Test {
-    
+    uint256 px = 81866261498145529725865355090981;
+    uint256 py = 99999999999999043067456379334985;
+    uint256 x0 = 1e26;
+    uint256 y0 = 3869516;
+    uint256 cx = 800290974685695047;
+    uint256 cy = 911725765025283277;
+
     /// @dev EulerSwap curve definition
     /// Pre-conditions: x <= x0, 1 <= {px,py} <= 1e36, {x0,y0} <= type(uint112).max, c <= 1e18
-    function fEulerSwap(uint256 x, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 c) internal pure returns (uint256) {
+    function fEulerSwap(uint256 x, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 c)
+        internal
+        pure
+        returns (uint256)
+    {
         unchecked {
             uint256 v = Math.mulDiv(px * (x0 - x), c * x + (1e18 - c) * x0, x * 1e18, Math.Rounding.Ceil);
             require(v <= type(uint248).max, "HELP");
@@ -18,52 +28,77 @@ contract EulerSwapScenarioTest is Test {
         }
     }
 
-    function fInverseEulerSwap(uint256 y, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 c) internal pure returns (uint256) {
-        int256 B = int256(Math.mulDiv(py, y - y0, px, Math.Rounding.Ceil)) - (2 * int256(c) - int256(1e18)) * int256(x0) / 1e18;
-        uint256 absB = abs(B);
-        uint256 squaredB = Math.mulDiv(absB, absB * 1e18, 1e18);
-        uint256 squaredX0 = Math.mulDiv(x0, x0, 1e18);        
+    function fInverseEulerSwap(uint256 y, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 c)
+        internal
+        pure
+        returns (uint256)
+    {
+        int256 B =
+            int256(Math.mulDiv(py, y - y0, px, Math.Rounding.Ceil)) - (2 * int256(c) - int256(1e18)) * int256(x0) / 1e18;
+        uint256 squaredX0 = Math.mulDiv(x0, x0, 1e18);
+        console.log("B", B);
         uint256 C = Math.mulDiv(uint256(int256(1e18) - int256(c)), squaredX0, 1e18);
-        uint256 discriminant = uint256(int256(squaredB) + 4 * int256(c) * int256(C));
-        uint256 sqrt = Math.sqrt(discriminant);
-        sqrt = (sqrt * sqrt < discriminant) ? sqrt + 1 : sqrt;
-        if (B < 0) {
-            return Math.mulDiv(uint256(int256(sqrt) - B), 1e18, 2 * c, Math.Rounding.Ceil) + 2;
+        console.log("C", C);
+        int256 maxAC = 2 * int256(Math.sqrt(Math.mulDiv(c * 1e18, C, 1)));
+        console.log("maxAC", maxAC);
+
+        if (B > maxAC) {
+            return uint256(B) * 1e18 / c;
         } else {
-            return Math.mulDiv(2 * C, 1e18, absB + sqrt, Math.Rounding.Ceil) + 2;
+            uint256 absB = abs(B);
+            uint256 squaredB = Math.mulDiv(absB, absB * 1e18, 1e18);
+            uint256 discriminant = uint256(int256(squaredB) + 4 * int256(c) * int256(C));
+            uint256 sqrt = Math.sqrt(discriminant);
+            sqrt = (sqrt * sqrt < discriminant) ? sqrt + 1 : sqrt;
+            if (B < 0) {
+                return Math.mulDiv(uint256(int256(sqrt) - B), 1e18, 2 * c, Math.Rounding.Ceil) + 2;
+            } else {
+                return Math.mulDiv(2 * C, 1e18, absB + sqrt, Math.Rounding.Ceil) + 2;
+            }
         }
     }
 
-    function verifyEulerSwap(uint256 x, uint256 y, uint256 x0, uint256 y0, uint256 px, uint256 py, uint256 cx, uint256 cy) public view returns (bool) {        
+    function verifyEulerSwap(
+        uint256 x,
+        uint256 y,
+        uint256 x0,
+        uint256 y0,
+        uint256 px,
+        uint256 py,
+        uint256 cx,
+        uint256 cy
+    ) public view returns (bool) {
         if (x > type(uint112).max || y > type(uint112).max) return false;
         if (x >= x0) {
             if (y >= y0) return true;
-            return
-                x >= fEulerSwap(y, py, px, y0, x0, cy);
+            return x >= fEulerSwap(y, py, px, y0, x0, cy);
         } else {
-            if (y < y0) return false;            
-            return
-                y >= fEulerSwap(x, px, py, x0, y0, cx);
+            if (y < y0) return false;
+            return y >= fEulerSwap(x, px, py, x0, y0, cx);
         }
     }
 
-    function scaleUpX(uint256 x, uint256 x0) internal pure returns (uint256) {        
+    function scaleUpX(uint256 x, uint256 x0) internal pure returns (uint256) {
         return Math.mulDiv(x, x0, 1e18);
     }
 
-    function scaleDownY(uint256 y, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 c) internal pure returns (uint256) {        
+    function scaleDownY(uint256 y, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 c)
+        internal
+        pure
+        returns (uint256)
+    {
         return Math.mulDiv(py * 1e18, y - y0, x0 * px) + 1e18;
     }
 
     // function getScaledY(uint256 x, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 c) internal pure returns (uint256) {
     //     uint256 p = px * 1e18 / py;
-    //     uint256 scaledX = (x * 1e18) / x0;        
+    //     uint256 scaledX = (x * 1e18) / x0;
     //     return y0 + Math.mulDiv(p * x0, f(scaledX, c) - 1e18, 1e36);
     // }
 
     // function getScaledX(uint256 y, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 c) internal pure returns (uint256) {
     //     uint256 p = px * 1e18 / py;
-    //     uint256 scaledY = (y - y0) * 1e36 / (p * x0) + 1e18;        
+    //     uint256 scaledY = (y - y0) * 1e36 / (p * x0) + 1e18;
     //     return quadratic(scaledY, c);
     // }
 
@@ -85,15 +120,18 @@ contract EulerSwapScenarioTest is Test {
 
     function quadratic(uint256 y, uint256 c) internal pure returns (uint256) {
         int256 B = int256(y) - 2 * int256(c);
-        console.log("B", B);
-        uint256 absB = abs(B);
-        console.log("absB", absB);
-        uint256 squaredB = PRBMath.mulDiv(absB, absB, 1);        
-        console.log("squaredB", squaredB);        
-        uint256 discriminant = squaredB + Math.mulDiv(4 * c, (1e18 - c) * 1e18, 1e18);
-        console.log("discriminant", discriminant);
-        uint256 sqrt = Math.sqrt(discriminant);
-        sqrt = (sqrt * sqrt < discriminant) ? sqrt + 1 : sqrt;
+        int256 maxAC = 2 * int256(Math.sqrt(Math.mulDiv(c * 1e18, 1e18 - c, 1)));
+        uint256 sqrt;
+        if (B > maxAC) {
+            sqrt = uint256(B);
+        } else {
+            uint256 absB = abs(B);
+            uint256 squaredB = absB * absB;
+            uint256 discriminant = squaredB + Math.mulDiv(4 * c, (1e18 - c) * 1e18, 1e18);
+            sqrt = Math.sqrt(discriminant);
+            sqrt = (sqrt * sqrt < discriminant) ? sqrt + 1 : sqrt;
+        }
+
         if (B < 0) {
             return Math.mulDiv(2 * c - y + sqrt, 1e18, 2 * c, Math.Rounding.Ceil) + 2;
         } else {
@@ -123,8 +161,8 @@ contract EulerSwapScenarioTest is Test {
     }
 
     function test_Binary() public {
-        uint256 cx = 0.1e18;
-        uint256 y = 3.3243e18;
+        uint256 cx = 1987860;
+        uint256 y = 742411638020043034953197016458887439;
         uint256 startGas = gasleft();
         uint256 x = binary(y, cx, 1, 1e18);
         uint256 endGas = gasleft();
@@ -143,75 +181,46 @@ contract EulerSwapScenarioTest is Test {
     function test_Quadratic() public {
         uint256 cx = 1987860;
         uint256 y = 742411638020043034953197016458887439;
-        uint256 yHalf = f(0.5e18, cx);
         uint256 startGas = gasleft();
         uint256 x = quadratic(y, cx);
         uint256 endGas = gasleft();
         uint256 gasUsed = startGas - endGas;
         uint256 yCalc = f(x, cx);
-        uint256 xBin = x;
-        console.log("y", y);
-        console.log("yCalc", yCalc);
-        console.log("x", x);
-        console.log("delta", int256(y) - int256(yCalc));
-        console.log("Gas used:", gasUsed);
-        console.log();
-
-        assertApproxEqAbs(x, xBin, 100);
-    }
-
-    function test_FuzzQuadraticBinary(uint256 y, uint256 cx) public {
-        // Params
-        cx = bound(cx, 1, 1e18);
-        uint256 yMax = f(1, cx);
-        y = bound(y, 1e18 + 1, yMax);
-        uint256 startGas = gasleft();
-        uint256 x = quadratic(y, cx);
-        uint256 endGas = gasleft();
-        uint256 gasUsed = startGas - endGas;
-        uint256 yCalc = f(x, cx);
-        console.log("y", y);
-        console.log("yCalc", yCalc);
-        console.log("x", x);
-        console.log("delta", int256(y) - int256(yCalc));
-        console.log("Gas used:", gasUsed);
-        console.log();
-
         uint256 xBin = binary(y, cx, 1, 1e18);
-        console.log("xBin", xBin);
-
-        assert(y >= f(x, cx));        
-    }
-
-    // Test if we start with a y value, scale it down, solve for x, do we get back an x that passes invariant and is close to the original x
-    function test_FuzzMappingMethod(uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 cx, uint256 cy) public {
-        // Params
-        px = bound(px, 1e18, 1e32);
-        py = bound(px, 1e18, 1e32);
-        x0 = 1e26;
-        y0 = 1e26;
-        cx = bound(cx, 1, 1e18);
-        cy = bound(cy, 1, 1e18);
-
-        uint256 x = 30.214324e18;
-        uint256 y = fEulerSwap(x, px, py, x0, y0, cx);
-        console.log("x", x);
         console.log("y", y);
+        console.log("yCalc", yCalc);
+        console.log("x", x);
+        console.log("delta", int256(y) - int256(yCalc));
+        console.log("Gas used:", gasUsed);
+        console.log();
 
-        uint256 yScaledDown = Math.mulDiv(y - y0, 1e36, Math.mulDiv(px, 1e18, py, Math.Rounding.Ceil) * x0, Math.Rounding.Floor) + 1e18;
-        console.log("yScaledDown", yScaledDown);
-
-        uint256 xScaledDownCalc = quadratic(yScaledDown, cx);
-        console.log("xScaledDownCalc", xScaledDownCalc);
-
-        uint256 xScaledUp = Math.mulDiv(xScaledDownCalc, x0, 1e18, Math.Rounding.Ceil);
-        console.log("xScaledUp", xScaledUp);
-
-        assert(verifyEulerSwap(xScaledUp, y, x0, y0, px, py, cx, cy));
-        // assertApproxEqAbs(x, xScaledUp, 200);
+        assertApproxEqAbs(x, xBin, 5);
+        assert(y >= f(xCalc, cx));
     }
 
-    // Test if we start with a y value, scale it down, solve for x, do we get back an x that passes invariant and is close to the original x
+    function test_FuzzQuadratic(uint256 x, uint256 cx) public {
+        x = bound(x, 1, 1e18);
+        cx = bound(cx, 1, 1e18);        
+        uint256 y = f(x, cx);
+        uint256 startGas = gasleft();
+        uint256 xCalc = quadratic(y, cx);
+        uint256 endGas = gasleft();
+        uint256 gasUsed = startGas - endGas;
+        uint256 yCalc = f(x, cx);
+        uint256 xBin = binary(y, cx, 1, 1e18);
+        console.log("y", y);
+        console.log("yCalc", yCalc);
+        console.log("x", x);
+        console.log("xCalc", xCalc);
+        console.log("xBin", xBin);
+        console.log("delta", int256(y) - int256(yCalc));
+        console.log("Gas used:", gasUsed);
+        console.log();
+
+        assertApproxEqAbs(x, xBin, 5);
+        assert(y >= f(xCalc, cx));
+    }
+
     function test_fInverse() public {
         // Params
         uint256 px = 24e18;
@@ -226,7 +235,7 @@ contract EulerSwapScenarioTest is Test {
         uint256 startGas = gasleft();
         uint256 xCalc = fInverseEulerSwap(y, px, py, x0, y0, cx);
         uint256 endGas = gasleft();
-        uint256 gasUsed = startGas - endGas;        
+        uint256 gasUsed = startGas - endGas;
         uint256 yCalc = fEulerSwap(xCalc, px, py, x0, y0, cx);
         console.log("x", x);
         console.log("y", y);
@@ -235,58 +244,91 @@ contract EulerSwapScenarioTest is Test {
         console.log("Gas used:", gasUsed);
     }
 
-    function test_fuzzfInverse(uint256 x, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 cx, uint256 cy) public {
-        // Params
-        x = bound(x, 0.05e18, 1e26 - 2); // TODO: note that the -2 here is the tolerance in the fInverse function. Without this, f() fails when x gets too close to centre.
-        console.log("x", x);
-        
-        px = bound(px, 1e18, 1e32);
-        py = bound(py, 1e18, 1e32);
-        x0 = 1e26;
-        y0 = bound(y0, 0, 1e26);
-        cx = bound(cx, 1, 1e18);
-        cy = bound(cy, 1, 1e18);
 
-        uint256 y = fEulerSwap(x, px, py, x0, y0, cx);
-        console.log("y", y);        
-        uint256 xCalc = fInverseEulerSwap(y, px, py, x0, y0, cx);
-        console.log("xCalc", xCalc);        
-        uint256 yCalc = fEulerSwap(xCalc, px, py, x0, y0, cx);        
-        console.log("yCalc", yCalc);
 
-        if (x < type(uint112).max && y < type(uint112).max){
-            assert(verifyEulerSwap(xCalc, y, x0, y0, px, py, cx, cy));
-            assert(abs(int(y) - int(yCalc)) < 4 || abs(int(x) - int(xCalc)) < 4);
-        }
-    }    
 
-    function test_fuzzfInverseScalingMethod(uint256 x, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 cx, uint256 cy) public {
-        // Params
-        x = bound(x, 0.05e18, 1e26 - 2); // TODO: note that the -2 here is the tolerance in the fInverse function. Without this, f() fails when x gets too close to centre.
-        console.log("x", x);
-        
-        px = bound(px, 1e18, 1e32);
-        py = bound(py, 1e18, 1e32);
-        x0 = 1e26;
-        y0 = bound(y0, 0, 1e26);
-        cx = bound(cx, 1, 1e18);
-        cy = bound(cy, 1, 1e18);
 
-        uint256 y = fEulerSwap(x, px, py, x0, y0, cx);
-        console.log("y", y);
-        uint256 yDown = scaleDownY(y, px, py, x0, y0, cx);
-        console.log("yDown", yDown);
-        uint256 xDownCalc = quadratic(y, cx);
-        console.log("xDownCalc", xDownCalc);
-        uint256 xCalc = scaleUpX(x, x0) / 1e8;
-        console.log("xCalc", xCalc);
-        console.log("xCheck", x == xCalc);
-        uint256 yCalc = fEulerSwap(xCalc, px, py, x0, y0, cx);        
-        console.log("yCalc", yCalc);
+    // // Test if we start with a y value, scale it down, solve for x, do we get back an x that passes invariant and is close to the original x
+    // function test_FuzzMappingMethod(uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 cx, uint256 cy) public {
+    //     // Params
+    //     px = bound(px, 1e18, 1e32);
+    //     py = bound(px, 1e18, 1e32);
+    //     x0 = 1e26;
+    //     y0 = 1e26;
+    //     cx = bound(cx, 1, 1e18);
+    //     cy = bound(cy, 1, 1e18);
 
-        if (x < type(uint112).max && y < type(uint112).max){
-            assert(verifyEulerSwap(xCalc, y, x0, y0, px, py, cx, cy));
-            assert(abs(int(y) - int(yCalc)) < 4 || abs(int(x) - int(xCalc)) < 4);
-        }
-    }    
+    //     uint256 x = 30.214324e18;
+    //     uint256 y = fEulerSwap(x, px, py, x0, y0, cx);
+    //     console.log("x", x);
+    //     console.log("y", y);
+
+    //     uint256 yScaledDown =
+    //         Math.mulDiv(y - y0, 1e36, Math.mulDiv(px, 1e18, py, Math.Rounding.Ceil) * x0, Math.Rounding.Floor) + 1e18;
+    //     console.log("yScaledDown", yScaledDown);
+
+    //     uint256 xScaledDownCalc = quadratic(yScaledDown, cx);
+    //     console.log("xScaledDownCalc", xScaledDownCalc);
+
+    //     uint256 xScaledUp = Math.mulDiv(xScaledDownCalc, x0, 1e18, Math.Rounding.Ceil);
+    //     console.log("xScaledUp", xScaledUp);
+
+    //     assert(verifyEulerSwap(xScaledUp, y, x0, y0, px, py, cx, cy));
+    //     // assertApproxEqAbs(x, xScaledUp, 200);
+    // }
+
+
+    // function test_fuzzfInverse(uint256 x, uint256 px, uint256 py, uint256 x0, uint256 y0, uint256 cx, uint256 cy)
+    //     public
+    // {
+    //     // Params
+    //     x = bound(x, 0.05e18, 1e26 - 2); // TODO: note that the -2 here is the tolerance in the fInverse function. Without this, f() fails when x gets too close to centre.
+    //     console.log("x", x);
+
+    //     px = bound(px, 1e18, 1e32);
+    //     py = bound(py, 1e18, 1e32);
+    //     x0 = 1e26;
+    //     y0 = bound(y0, 0, 1e26);
+    //     cx = bound(cx, 1, 1e18);
+    //     cy = bound(cy, 1, 1e18);
+
+    //     uint256 y = fEulerSwap(x, px, py, x0, y0, cx);
+    //     console.log("y", y);
+    //     uint256 xCalc = fInverseEulerSwap(y, px, py, x0, y0, cx);
+    //     console.log("xCalc", xCalc);
+    //     uint256 yCalc = fEulerSwap(xCalc, px, py, x0, y0, cx);
+    //     console.log("yCalc", yCalc);
+
+    //     if (x < type(uint112).max && y < type(uint112).max) {
+    //         assert(verifyEulerSwap(xCalc, y, x0, y0, px, py, cx, cy));
+    //         assert(abs(int256(y) - int256(yCalc)) < 4 || abs(int256(x) - int256(xCalc)) < 4);
+    //     }
+    // }
+
+    // function test_fuzzfInverseScalingMethod() public {
+    //     // Params
+    //     uint256 x = 139780697298741147996; // TODO: note that the -2 here is the tolerance in the fInverse function. Without this, f() fails when x gets too close to centre.
+    //     console.log("x", x);
+    //     console.log("xScaled", x * 1e18 / x0);
+
+    //     uint256 yMax = f(2, cx);
+    //     console.log("yMax", yMax);
+
+    //     uint256 y = fEulerSwap(x, px, py, x0, y0, cx);
+    //     console.log("y", y);
+    //     uint256 yDown = scaleDownY(y, px, py, x0, y0, cx);
+    //     console.log("yDown", yDown);
+    //     uint256 xDownCalc = quadratic(y, cx);
+    //     console.log("xDownCalc", xDownCalc);
+    //     uint256 xCalc = scaleUpX(x, x0) / 1e8;
+    //     console.log("xCalc", xCalc);
+    //     console.log("xCheck", x == xCalc);
+    //     uint256 yCalc = fEulerSwap(xCalc, px, py, x0, y0, cx);
+    //     console.log("yCalc", yCalc);
+
+    //     if (x < type(uint112).max && y < type(uint112).max) {
+    //         assert(verifyEulerSwap(xCalc, y, x0, y0, px, py, cx, cy));
+    //         assert(abs(int256(y) - int256(yCalc)) < 4 || abs(int256(x) - int256(xCalc)) < 4);
+    //     }
+    // }
 }
