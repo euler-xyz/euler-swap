dd14ea3c394e556b005ef801d46403b2650f87e2
saucepoint <saucepoint@protonmail.com>
2025-04-03 12:46:14 -0400

    add limits

--- src/base/ProtocolFee.sol
+++ src/base/ProtocolFee.sol
@@ -8,9 +8,14 @@ abstract contract ProtocolFee is Owned {
     uint256 public protocolFee;
     address public protocolFeeRecipient;
 
+    error InvalidFee();
+
     constructor(address _feeOwner) Owned(_feeOwner) {}
 
+    /// @notice Set the protocol fee, expressed as a percentage of LP fee
+    /// @param newFee The new protocol fee, in WAD units (0.10e18 = 10%)
     function setProtocolFee(uint256 newFee) external onlyOwner {
+        if (newFee > (1e18 - 1)) revert InvalidFee();
         protocolFee = newFee;
     }
 
